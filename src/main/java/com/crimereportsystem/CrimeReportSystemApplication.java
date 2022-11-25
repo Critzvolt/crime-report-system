7baaa9a8bf9e25e57d2eb24792c8c948724ece0f
Critzvolt <72713966+Critzvolt@users.noreply.github.com>
2022-11-25 15:03:10 +0100

    first commit

---
+++ src/main/java/com/crimereportsystem/CrimeReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.crimereportsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crimereportsystem/CrimeReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimereportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
